31df774cb4bdf2cc944a22ef0624cdcd8e5bedf0
Giacomo Fulvi <giacomofulvi@MBP-Summer6.local>
2024-03-05 17:53:47 +0100

    added datasetup (#2)

--- srcs/requirements/mariadb/tools/datasetup.sh
+++ srcs/requirements/mariadb/tools/datasetup.sh
@@ -1,1 +1,9 @@
-echo ciao
+#!/bin/bash
+
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $DATABASE;"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysql -e "FLUSH PRIVILEGES;"
